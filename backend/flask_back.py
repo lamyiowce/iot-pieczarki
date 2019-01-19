672ca7d33863666247988ced7b0d203c7422a9d7
Julia Bazińska <julia@bazinski.com>
2019-01-19 19:05:16 +0100

    some mock data in db, added growth check, regular data check in frontend, etc

--- backend/flask_back.py
+++ backend/flask_back.py
@@ -33,5 +33,26 @@ def stats(cultivation):
         res[t] = []
         for row in c:
             res[t].append({"date": row[0], "value": row[1]})
+            print(res)
     return json.dumps(res)
 
+
+@app.route('/freq/<string:type>/<int:value>')
+def freq(type, value):
+    import time, random
+    print(type, value)
+    time.sleep(2)
+    r = random.random()
+    print(r)
+    if r > 0.5:
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False}), 500, {'ContentType': 'application/json'}
+
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
