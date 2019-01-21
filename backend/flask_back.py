a9e716bf11dd47f14b8cdc63fb2092259000ba74
Julia Bazińska <julia@bazinski.com>
2019-01-21 20:35:07 +0100

    some mock data in db, added growth check, regular data check in frontend, etc

--- backend/flask_back.py
+++ backend/flask_back.py
@@ -22,11 +22,13 @@ def cultivations():
 def stats(cultivation):
     conn = db.connect('database')
     types = conn.cursor()
-    types.execute('SELECT name,id FROM types')
-    res = {}
+    types.execute('SELECT name,id,interval FROM types')
+    res = {'interval': {}}
     for typ in types:
         t = typ[0]
         tid = typ[1]
+        interval = typ[2]
+        res['interval'][t] = interval
         c = conn.cursor()
         sql = 'SELECT date, value FROM stats WHERE type = ' + str(tid) + ' AND cultivation = ' + str(cultivation) + ' ORDER BY datetime(date)'
         c.execute(sql)
@@ -37,22 +39,18 @@ def stats(cultivation):
     return json.dumps(res)
 
 
-@app.route('/freq/<string:type>/<int:value>')
+@app.route('/freq/<string:type>/<int:value>/')
 def freq(type, value):
-    import time, random
-    print(type, value)
-    time.sleep(2)
-    r = random.random()
-    print(r)
-    if r > 0.5:
-        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-    else:
-        return json.dumps({'success': False}), 500, {'ContentType': 'application/json'}
+    conn = db.connect('database')
+    c = conn.cursor()
+    sql = "UPDATE types SET interval = " + str(value) + " WHERE (name LIKE '" + type + "%');"
+    c.execute(sql)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
